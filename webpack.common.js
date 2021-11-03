4305d8a49d686596f47df9c344a10b1b67f44d51
OhVIton <vim.usenoarrows@gmail.com>
2021-11-03 16:06:47 +0900

    fixed build options

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,19 @@ module.exports = {
   entry: {
     fetch_homework: './src/fetch_homework.js',
     show_homework: './src/show_homework.js',
-    background: './src/background.js'
+    background: './src/background.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json' },
+      ]
+    }),
+  ],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: '[name].js',
   },
   watchOptions: {
     ignored: /node_modules/,
--- webpack.dev.js
+++ webpack.dev.js
@@ -10,10 +10,24 @@ module.exports = (env, argv) => {
       mode: 'development',
       devtool: 'inline-source-map',
       plugins: [
+        // watch モードのみ Hot Reload を有効化
+        //  https://github.com/webpack/webpack/issues/6460#issuecomment-364252873
+        //  https://github.com/webpack/webpack/issues/6460#issuecomment-386947990
         (argv.watch || argv.w) ? (new ExtensionReloader({
           port: 9090,
           reloadPage: true,
           manifest: path.resolve(__dirname, 'src', 'manifest.json'),
+          entries: {
+            contentScript: [
+              'content',
+            ],
+            background: [
+              'background',
+            ],
+            extensionPage: [
+              'options',
+            ],
+          },
         })) : false,
       ].filter(Boolean),
     }
